d559f8549fa67f1ac9ba4ab220f078ed99a5f0e8
DanielFsR <danielferreirasantosd12@gmail.com>
2023-07-16 12:55:36 -0300

    Arrumando Controller e rotas/4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  get 'recipes/search', to: 'recipes#search'
   resources :recipe_ingredients
   resources :ingredients
   resources :recipes
-  get 'recipes/search', to: 'recipes#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
